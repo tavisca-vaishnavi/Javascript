4600df3fc77eaefec5b7d49ffee4d1637ea15053
Vaishnavi <vgalgali@tavisca.com>
2014-07-28 10:44:25 +0530

    Changes Made

--- javascript.js
+++ javascript.js
@@ -1,53 +1,73 @@
-﻿function demo(choice) {
+﻿
+
+
+var callLength = function () {
+    var ip1 = document.getElementById("string1").value;
+    var string_obj1 = new string_manipulation(ip1);
+    var res = document.getElementById("result");
+    var len = string_obj1.strlen;
+    res.innerHTML = len;
+}
+var callConcat = function () {
+    var ip1 = document.getElementById("string1").value;
+    var ip2 = document.getElementById("string2").value;
+    var string_obj1 = new string_manipulation(ip1);
+    var string_obj2 = new string_manipulation(ip2);
+    var res = document.getElementById("result");
+    var con_cat = string_obj1.concat(string_obj2);
+    res.innerHTML = con_cat;
+}
+var callSubstring = function () {
     var ip1 = document.getElementById("string1").value;
     var ip2 = document.getElementById("string2").value;
     var ip3 = document.getElementById("string3").value;
     var string_obj1 = new string_manipulation(ip1);
     var string_obj2 = new string_manipulation(ip2);
     var string_obj3 = new string_manipulation(ip3);
-    var res = document.getElementById("output");
-    var output = new string_manipulation(res);
-
-    if (choice == "btn-Length") {
-        var len = string_obj1.strlen;
-
-        res.value = len;
-    }
-    if (choice == "btn-concat") {
-        var con_cat = string_obj1.concat(string_obj2);
-        res.value = con_cat;
-
-    }
-    if (choice == "btn-charat") {
-        var char = string_obj1.char_at(string_obj2);
-        res.value = char;
-
-    }
-    if (choice == "btn-indexof") {
-        var char = string_obj1.index_of(string_obj2);
-        res.value = char;
-
-    }
-    if (choice == "btn-Replace") {
-        var char = string_obj1.Replace(string_obj2);
-        res.value = char;
-
-    }
-    if (choice == "btn-substring") {
-        var char = string_obj1.mySubString(string_obj2, string_obj3);
-        res.value = char;
-
-    }
-    if (choice == "btn-last_index") {
-        var char = string_obj1.mySubString(string_obj2);
-        res.value = char;
-
-    }
-
-
-
+    var res = document.getElementById("result");
+    var char = string_obj1.mySubString(string_obj2, string_obj3);
+    res.innerHTML = char;
+}
+var callCharAT = function () {
+    var ip1 = document.getElementById("string1").value;
+    var ip2 = document.getElementById("string2").value;
+    var string_obj1 = new string_manipulation(ip1);
+    var string_obj2 = new string_manipulation(ip2);
+    var res = document.getElementById("result");
+    var char = string_obj1.char_at(string_obj2);
+    res.innerHTML = char;
 }
 
+var callLastIndex = function () {
+    var ip1 = document.getElementById("string1").value;
+    var ip2 = document.getElementById("string2").value;
+    var string_obj1 = new string_manipulation(ip1);
+    var string_obj2 = new string_manipulation(ip2);
+    var res = document.getElementById("result");
+    var char = string_obj1.getLastIndexOf(string_obj2);
+    res.innerHTML = char;
+}
+
+var callIndexOf = function () {
+    var ip1 = document.getElementById("string1").value;
+    var ip2 = document.getElementById("string2").value;
+    var string_obj1 = new string_manipulation(ip1);
+    var string_obj2 = new string_manipulation(ip2);
+    var res = document.getElementById("result");
+    var char = string_obj1.index_of(string_obj2);
+    res.innerHTML = char;
+}
+
+var callReplace = function () {
+    var ip1 = document.getElementById("string1").value;
+    var ip2 = document.getElementById("string2").value;
+    var string_obj1 = new string_manipulation(ip1);
+    var string_obj2 = new string_manipulation(ip2);
+    var res = document.getElementById("result");
+    var char = string_obj1.Replace(string_obj2);
+    res.innerHTML = char;
+
+}
 
 
 var string_manipulation = function (val) {
@@ -69,7 +89,9 @@ var string_manipulation = function (val) {
             if (this.value[i] == str.value) {
                 return i;
             }
+            
         }
+        return -1;
     }
     this.Replace = function (str) {
         var ret = "";
@@ -109,27 +131,24 @@ var mylength = function () {
 }
 
 
+var e = document.getElementById("btn-Length");
+e.onclick = callLength;
 
+var e = document.getElementById("btn-concat");
+e.onclick = callConcat;
 
-/*var x = function (val){
-    this.value = val;
-    this.mylength = function () {
-        var i = 0, len = 0;
-        var string = document.getElementById("string1").value;
-        for (i = 0; string[i] != null; i++) {
-            len++;
-        }
-        var TheTextBox = document.getElementById("output");
-        TheTextBox.value = TheTextBox.value + len;
-    }
-  
-    var concat = function(){
-        var string1 = document.getElementById("string1").value;
-        var string2 = document.getElementById("string2").value;
-        var final;
-        final = string1 + string2;
-        var TheTextBox = document.getElementById("output");
-        TheTextBox.value = TheTextBox.value + final;
+var e = document.getElementById("btn-substring");
+e.onclick = callSubstring;
 
-    }
-}*/
+var e = document.getElementById("btn-charat");
+e.onclick = callCharAT;
+
+var e = document.getElementById("btn-last_index");
+e.onclick = callLastIndex;
+
+var e = document.getElementById("btn-indexof");
+e.onclick = callIndexOf;
+
+var e = document.getElementById("btn-Replace");
+e.onclick = callReplace;
+
