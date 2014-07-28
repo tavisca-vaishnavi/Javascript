53f220af41e04acd5cf188bbadcf0691535f3c04
Vaishnavi <vgalgali@tavisca.com>
2014-07-28 11:23:29 +0530

    ball using canvas

---
+++ ball/ball.js
@@ -0,0 +1,24 @@
+﻿var bounch;
+var dx = 5;
+var dy = 5;
+var x = 100;
+var y = 200;
+function init() {
+   
+    var b = document.getElementById("myball");
+    bounch = b.getContext('2d');
+    setInterval(paint, 10);
+    
+}
+function paint() {
+    bounch.clearRect(0, 0, 800, 600);
+    bounch.beginPath();
+    bounch.fillStyle = "#FF00FF";
+    bounch.arc(x, y, 20, 0, Math.PI * 2, true);
+    bounch.closePath();
+    bounch.fill();
+    if (x < 0 || x > 800) dx = -dx;
+    if (y < 0 || y > 600) dy = -dy;
+    x =x + dx;
+    y =y + dy;
+}
